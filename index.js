bceab457a319256df47453e365e2934693dd2ac4
Josbell Quiros <josbell.quiros@oasisdigital.com>
2025-01-28 16:12:48 -0800

    Get todos from mongodb feature Add todo feature Refactor implement mongodb service
    Co-authored-by: Marco Molina <marcomolina11@gmail.com>

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 const express = require('express');
+const { getTodos, addTodo } = require('./mongoService');
 const cors = require('cors');
 const app = express();
 const port = 3000;
 
 app.use(cors());
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.status(200).json([]);
+app.get('/', async (req, res) => {
+  try {
+    const todos = await getTodos();
+    res.status(200).json(todos);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.post('/', async (req, res) => {
+  try {
+    const todo = req.body;
+    const _id = await addTodo(todo);
+    res.status(200).json({ ...todo, _id });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 app.listen(port, () => {
